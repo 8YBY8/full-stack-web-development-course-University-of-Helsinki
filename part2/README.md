4c4f6b8a0209926f14a15dd409afc6c5fc5a28bf
8YBY8 <yubingyang9@gmail.com>
2024-07-07 16:29:37 +0800

    2.11 finished

--- part2/README.md
+++ part2/README.md
@@ -3,4 +3,4 @@ Solutions of part 2 exercises to this folder, one app per folder
 - the project courseinfo for exercises 2.1 - 2.5 as folder courseinfo
  
 
-10 hrs 
+11.5 hrs 
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
+
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   // To enable editing of the input element, we have to register an event handler
   // that synchronizes the changes made to the input with the component's state
   const handleNameChange = (event) => {
-    // console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    // console.log(event.target.value);
     setNewNumber(event.target.value)
   }
 
   const handleFilterChange = (event) => {
-    // console.log(event.target.value);
     setFilter(event.target.value)
     filter === '' ? setShowAll(true) : setShowAll(false)
   }
